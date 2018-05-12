d393c8b3e2a3a92feb482ec47ba77be3b7b02434
Max Golik <maximko1312@gmail.com>
2018-05-12 18:02:41 +0300

    laba 3-2 is comleted

--- laba3-2/prog3-2.cs
+++ laba3-2/prog3-2.cs
@@ -2,11 +2,64 @@
 
 namespace laba3_2
 {
+    class Person
+    {
+        protected String aboutMe;
+        
+        public Person()
+        {
+            this.aboutMe = "I'm a Person";
+        }
+
+        public virtual void whoIsMe()
+        {
+            Console.WriteLine(aboutMe);
+        }
+    }
+
+    class Boy : Person
+    {
+        public Boy()
+        {
+            this.aboutMe = "I'm a Boy";
+        }
+
+        public override void whoIsMe()
+        {
+            base.whoIsMe();
+        }
+    }
+
+    class Girl : Person
+    {
+        public Girl()
+        {
+            this.aboutMe = "I'm a Girl";
+        }
+
+        public override void whoIsMe()
+        {
+            base.whoIsMe();
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = new Person();
+            Person boy = new Boy();
+            Person girl = new Girl();
+            
+            Person[] masOfPerson = new Person[3];
+            masOfPerson[0] = person;
+            masOfPerson[1] = boy;
+            masOfPerson[2] = girl;
+
+            for (int i = 0; i < masOfPerson.Length; i++)
+            {
+                masOfPerson[i].whoIsMe();
+            }
         }
     }
 }
